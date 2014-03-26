14035eaf00506d773218719135317327ce89f4ac
Kerry Buckley <kerryjbuckley@gmail.com>
2014-03-26 13:29:48 +0000

    End of week 2

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,32 +2,28 @@ MAXIMUM_QUALITY = 50
 
 def update_quality(items)
   items.each do |item|
-    if increasing_quality?(item) 
-      increase_quality(item)
-    elsif !legendary?(item) 
-      decrease_quality(item)
-    end
-
-    if item.sell_in < 1
-      if brie?(item)
-        increase_quality(item)
-      else
-        if backstage_pass?(item)
-          item.quality = 0
-        elsif !legendary?(item)
-          decrease_quality(item)
-        end
-      end
-    end
-
-    item.sell_in -= 1 unless legendary?(item)
+    adjust_quality(item)
+    decrease_sell_in(item)
   end
 end
 
 private
 
+def adjust_quality(item)
+  increase_quality(item)
+  decrease_quality(item)
+end
+
+def decrease_sell_in(item)
+  item.sell_in -= 1 unless legendary?(item)
+end
+
 def increasing_quality?(item)
   brie?(item) || backstage_pass?(item)
+end
+
+def non_degrading?(item)
+  legendary?(item) || brie?(item)
 end
 
 def legendary?(item)
@@ -43,15 +39,23 @@ def brie?(item)
 end
 
 def increase_quality(item)
+  return unless increasing_quality?(item)
   item.quality = [MAXIMUM_QUALITY, item.quality + quality_increment_for(item)].min
 end
 
 def decrease_quality(item)
-  item.quality -= 1 unless item.quality <= 0
+  if backstage_pass?(item)
+    item.quality = 0 if item.sell_in < 1
+  else
+    decrement = item.sell_in < 1 ?  2 : 1
+    item.quality -= decrement unless item.quality <= 0 || non_degrading?(item)
+  end
 end
 
 def quality_increment_for(item)
-  if backstage_pass?(item)
+  if brie?(item) && item.sell_in < 1
+    return 2
+  elsif backstage_pass?(item)
     if item.sell_in < 6
       return 3
     elsif item.sell_in < 11
